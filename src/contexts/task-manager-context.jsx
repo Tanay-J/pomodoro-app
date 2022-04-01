fd6e97de3d8586b9ed16abd9c3717283421c8203
Tanay <tanay.joshi9@gmail.com>
2022-04-02 00:00:26 +0530

    feat: add context and reducer function to manage tasks

---
+++ src/contexts/task-manager-context.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useReducer } from "react";
+import { taskManagerReducer } from "../utils/reducers/taskManagerReducer";
+
+const TaskManagerContext = createContext();
+const useTaskManager = () => useContext(TaskManagerContext);
+
+const TaskManagerProvider = ({ children }) => {
+  const initialState = {
+    taskList: [],
+    showModal: false,
+    showTimer: false,
+    taskToEdit: {},
+  };
+
+  const [taskManagerState, taskManagerDispatch] = useReducer(
+    taskManagerReducer,
+    initialState
+  );
+  return (
+    <TaskManagerContext.Provider
+      value={{ taskManagerState, taskManagerDispatch }}
+    >
+      {children}
+    </TaskManagerContext.Provider>
+  );
+};
+
+export { TaskManagerProvider, useTaskManager };
