ec5d20603b539cc4c8875d2d0a9f9e518bc8811b
Tanay <tanay.joshi9@gmail.com>
2022-04-01 23:58:10 +0530

    feat, style: add Homepage for pomodoro app

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,4 +1,40 @@
+import { TaskList } from "../home/TaskList";
+import { BsPlusCircleFill } from "react-icons/bs";
+import { Modal } from "../modal/Modal";
+import { useTaskManager } from "../../contexts/task-manager-context";
+import { Navbar } from ".././navigation/";
+import styles from "./home.module.css";
+
 const HomePage = () => {
-  return <h2>Homepage</h2>;
+  const { taskManagerState, taskManagerDispatch } = useTaskManager();
+  return (
+    <>
+      <Navbar />
+      <div className="m-xl">
+        <h3 className="h3 text-dark">Welcome, Tanay</h3>
+        {taskManagerState.taskList.length ? (
+          <p className="text-s text-gray">
+            You have {taskManagerState.taskList.length} tasks pending. Let's go!
+          </p>
+        ) : (
+          <p className="text-s text-gray">Let's start!</p>
+        )}
+      </div>
+
+      <div className={`${styles.tasks_container} mx-xl p-m br-m`}>
+        <div className="flex justify-con-space-bet">
+          <h5 className="h5 text-primary">Tasks</h5>
+          <BsPlusCircleFill
+            size={40}
+            className="pointer text-primary"
+            onClick={() => taskManagerDispatch({ type: "TOGGLE_MODAL" })}
+          />
+          {taskManagerState.showModal && <Modal />}
+        </div>
+
+        <TaskList />
+      </div>
+    </>
+  );
 };
 export { HomePage };
