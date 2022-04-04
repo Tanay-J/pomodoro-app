dfadbd25077da94570434e35ff21e5a4c58a230a
Tanay <tanay.joshi9@gmail.com>
2022-04-04 13:58:40 +0530

    feat,style: dark theme support and local storage for theme

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,13 +1,44 @@
-import { BsLightbulbFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import styles from "./navigation.module.css";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkMode")) || false
+  );
+
+  useEffect(() => {
+    darkMode
+      ? document.body.classList.add("dark-mode")
+      : document.body.classList.remove("dark-mode");
+  }, [darkMode]);
+
   return (
-    <header class="nav-header nav-bg-transparent px-xl py-xs">
-      <h2 class="h2 text-primary">Streak.</h2>
+    <header
+      className={`${
+        darkMode && styles.nav_header_dark
+      } nav-header nav-bg-transparent px-xl py-xs`}
+    >
+      <Link to="/home" className="link">
+        <h2 class="h2 text-primary">Streak.</h2>
+      </Link>
       <nav>
-        <ul class="nav-links">
+        <ul className="nav-links">
           <li>
-            <BsLightbulbFill size={25} className="pointer" />
+            {darkMode ? (
+              <BsSunFill
+                size={25}
+                className="text-white pointer"
+                onClick={() => setDarkMode(false)}
+              />
+            ) : (
+              <BsMoonFill
+                size={25}
+                className="pointer"
+                onClick={() => setDarkMode(true)}
+              />
+            )}
           </li>
         </ul>
       </nav>
